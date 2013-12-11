ac50188e1848d7d9780f281962af3bfee86f2f23
davidhurley <dhurls99@gmail.com>
2013-12-11 11:51:58 -0500

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211155528) do
+ActiveRecord::Schema.define(version: 20131211163129) do
 
   create_table "pins", force: true do |t|
     t.string   "story"
